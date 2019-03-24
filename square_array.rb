02f3cdfad14b867600f10ce0629358daa017d01e
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-24 21:14:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
  def square_array(array)
  numbers = [1,2,3]
- numbers.each_with_index([]) { |numbers,arr| arr << numbers ** 2 }
+ numbers.each_with_index { |numbers,arr| arr << numbers ** 2 }
+ 
+ a = [ "a", "b", "c" ]
+a.each_index {|x| print x, " -- " }
+
   end
 end
